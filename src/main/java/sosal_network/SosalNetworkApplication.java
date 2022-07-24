84bdfbaa24d319678d1ec42e029687e7a7f4c374
Nikita <nzhigulevskiy@bk.ru>
2022-07-24 19:08:40 +0300

    initial commit

---
+++ src/main/java/sosal_network/SosalNetworkApplication.java
@@ -0,0 +1,13 @@
+package sosal_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosalNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosalNetworkApplication.class, args);
+	}
+
+}
